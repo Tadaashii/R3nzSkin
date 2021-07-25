da40c3a841dfa339327a5b63a125a3eb54fe494c
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2021-07-25 15:32:24 +0300

    Reformat encryption.hpp with my own code style

--- R3nzSkin/skin_database.hpp
+++ R3nzSkin/skin_database.hpp
@@ -23,7 +23,7 @@ namespace skin_database {
 		std::vector<std::string> skins;
 	};
 
-	void load();
+	void load() noexcept;
 
 	extern std::map<uint32_t, std::vector<skin_info>> champions_skins;
 	extern std::vector<std::pair<uint32_t, std::string>> wards_skins;
