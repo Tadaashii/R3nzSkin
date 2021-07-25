b0f2ba5b5bc3fb090296a38c8d19fc70f5419e99
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2021-07-25 14:48:22 +0300

    Set .utf8 locale on initalization. Do not reset locale when converting skin names

--- R3nzSkin/dllmain.cpp
+++ R3nzSkin/dllmain.cpp
@@ -1,12 +1,14 @@
 #include <Windows.h>
+#include <clocale>
 
 #include "skin_changer.hpp"
 
-BOOL APIENTRY DllMain(HMODULE h_module, DWORD reason, LPVOID lp_reserved)
+BOOL APIENTRY DllMain(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 {
 	if (reason == DLL_PROCESS_ATTACH) {
-		DisableThreadLibraryCalls(h_module);
-		skin_changer::my_module = h_module;
+		std::setlocale(LC_CTYPE, ".utf8");
+		DisableThreadLibraryCalls(moduleHandle);
+		skin_changer::my_module = moduleHandle;
 		CreateThread(NULL, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(skin_changer::init), NULL, 0, NULL);
 	}
 	return TRUE;
